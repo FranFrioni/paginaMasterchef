250f22d76b12396107eb0e4e4cab698412f2284b
FranFrioni <francescofrioni@gmail.com>
2022-01-14 21:13:33 -0300

    Proyecto Masterchef

---
+++ script.js
@@ -0,0 +1,20 @@
+$(document).ready(function (){
+  $('#plata').popover();
+})
+$(document).ready(function (){
+  $('#crandongastronomico').popover();
+})
+$(document).ready(function (){
+  $('#gironaespaña').popover();
+})
+$(document).ready(function (){
+  $('#fiatargo').popover();
+})
+$(document).ready(function (){
+  $('#trofeo').popover();
+})
+
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  })
